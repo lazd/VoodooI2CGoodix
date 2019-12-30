43cf7eb63757ea776f30900c465a219336a05878
Larry Davis <lazdnet@gmail.com>
2019-12-30 11:18:37 -0800

    Add back code

--- VoodooI2CGoodix/VoodooI2CGoodixTouchDriver.cpp
+++ VoodooI2CGoodix/VoodooI2CGoodixTouchDriver.cpp
@@ -7,35 +7,62 @@
 //
 
 #include "VoodooI2CGoodixTouchDriver.hpp"
+#include "goodix.h"
 
 #define super IOService
 OSDefineMetaClassAndStructors(VoodooI2CGoodixTouchDriver, IOService);
 
 bool VoodooI2CGoodixTouchDriver::init(OSDictionary *properties) {
-    bool res = super::init(properties);
-    IOLog("Initializing\n");
-    return res;
+    transducers = NULL;
+    if (!super::init(properties)) {
+        return false;
+    }
+    transducers = OSArray::withCapacity(GOODIX_MAX_CONTACTS);
+    if (!transducers) {
+        return false;
+    }
+    DigitiserTransducerType type = kDigitiserTransducerFinger;
+    for (int i = 0; i < GOODIX_MAX_CONTACTS; i++) {
+        VoodooI2CDigitiserTransducer* transducer = VoodooI2CDigitiserTransducer::transducer(type, NULL);
+        transducers->setObject(transducer);
+    }
+    awake = true;
+    ready_for_input = false;
+    read_in_progress = false;
+    return true;
 }
 
 void VoodooI2CGoodixTouchDriver::free() {
-    IOLog("Freeing\n");
+    IOLog("%s::Freeing\n", getName());
     super::free();
 }
 
-VoodooI2CGoodixTouchDriver *VoodooI2CGoodixTouchDriver::probe(IOService* provider, SInt32* score) {
-    super::probe(provider, score);
-    IOLog("Probing\n");
+VoodooI2CGoodixTouchDriver* VoodooI2CGoodixTouchDriver::probe(IOService* provider, SInt32* score) {
+    IOLog("%s::Probing\n", getName());
+    if (!super::probe(provider, score)) {
+        return NULL;
+    }
+    acpi_device = OSDynamicCast(IOACPIPlatformDevice, provider->getProperty("acpi-device"));
+    if (!acpi_device) {
+        IOLog("Could not get ACPI device\n");
+        return NULL;
+    }
+    api = OSDynamicCast(VoodooI2CDeviceNub, provider);
+    if (!api) {
+        IOLog("Could not get VoodooI2C API instance\n");
+        return NULL;
+    }
     return this;
 }
 
 bool VoodooI2CGoodixTouchDriver::start(IOService* provider) {
     bool res = super::start(provider);
-    IOLog("Starting\n");
+    IOLog("%s::Starting\n", getName());
     return res;
 }
 
 void VoodooI2CGoodixTouchDriver::stop(IOService* provider) {
-    IOLog("Stopping\n");
+    IOLog("%s::Stopping\n", getName());
     super::stop(provider);
 }
 
--- VoodooI2CGoodix/VoodooI2CGoodixTouchDriver.hpp
+++ VoodooI2CGoodix/VoodooI2CGoodixTouchDriver.hpp
@@ -14,10 +14,13 @@
 #include "../../../Multitouch Support/VoodooI2CMultitouchInterface.hpp"
 #include "../../../Multitouch Support/MultitouchHelpers.hpp"
 #include "../../../Dependencies/helpers.hpp"
+#include "goodix.h"
 
 class VoodooI2CGoodixTouchDriver : public IOService {
     OSDeclareDefaultStructors(VoodooI2CGoodixTouchDriver);
 
+    VoodooI2CDeviceNub *api;
+    IOACPIPlatformDevice *acpi_device;
 public:
     /* Initialises the VoodooI2CGoodixTouchDriver object/instance (intended as IOKit driver ctor)
      *
@@ -33,7 +36,7 @@ public:
      *
      * @return returns an instance of the current VoodooI2CGoodixTouchDriver if there is a matched Goodix device, NULL otherwise
      */
-    VoodooI2CGoodixTouchDriver *probe(IOService* provider, SInt32* score) override;
+    VoodooI2CGoodixTouchDriver* probe(IOService* provider, SInt32* score) override;
     /* Starts the driver and initialises the Goodix device
      *
      * @return returns true if the driver has started
@@ -47,6 +50,16 @@ public:
 protected:
     IOReturn setPowerState(unsigned long powerState, IOService* whatDevice) override;
 
+    private:
+        bool awake;
+        bool read_in_progress;
+        bool ready_for_input;
+
+        IOCommandGate* command_gate;
+        IOInterruptEventSource* interrupt_source;
+        VoodooI2CMultitouchInterface *mt_interface;
+        OSArray* transducers;
+        IOWorkLoop* workLoop;
 };
 
 #endif /* VoodooI2CGoodixTouchDriver_hpp */
